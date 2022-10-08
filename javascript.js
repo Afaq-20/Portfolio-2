b4d12a8d3c682a19cff88985bfcfb3aa14a61469
Afaq Ahmad <afaq.xyz21@gmail.com>
2022-10-08 17:04:29 +0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let icon=document.getElementsById("icon")
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png"
+    }
+    else{
+        icon.src = "images/moon.png"
+    }
+}
